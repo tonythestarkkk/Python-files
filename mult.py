9dda8471e75bb5511e73d726191e3434ad790bb1
pramod kumar <43026017+tonythestarkkk@users.noreply.github.com>
2018-09-22 12:43:38 +0530

    Add files via upload

---
+++ mult.py
@@ -0,0 +1,4 @@
+#multiplication
+x=int(input("enter a number"))
+y=int(input("enter another number"))
+print(x,"*",y,"=",x*y)
---
+++ prom.py
@@ -0,0 +1,7 @@
+#check the number is positive or negtive
+a=int(input("enter a value a:"))
+b=int(input("enter a value b:"))
+if(a>b):
+	print("A is greater than B")
+else:
+	print("B is greater than A")
